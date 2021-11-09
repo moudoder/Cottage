f94af25aee7f987bda04453d6e5672e6d4f93b8c
moudoder <39887984+moudoder@users.noreply.github.com>
2021-11-09 16:56:00 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -22,15 +22,51 @@ $(document).ready(function () {
     focusOnSelect: true
    });
   $('.slider').slick({
-    infinite: true,
+    infinite: false,
     slidesToShow: 1,
     slidesToScroll: 1,
     swipe: true,
     arrows: false,
    });
   
+  let $element = $('.banking');
+  let counter = 0;
+
+  var h = document.getElementById('hg-wrap').scrollHeight;
+  
+  let hg_bd = $('.hg-wrap').height();
+  $('body').height(hg_bd)
+
+  $(window).scroll(function() {
+    let scroll = $(window).scrollTop() + $(window).height();
+    let offset = $element.offset().top
+   
+    if (scroll > offset && counter == 0) {
+      
+      counter = 1;
+    }
+  });
+
   $('.contacts-block').on('click', function() {
     $(this).toggleClass('contacts-block__active');
   })
 
+
+  $(window).bind('mousewheel DOMMouseScroll', function(event){
+      if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+          // scroll up
+      }
+      else {
+        if (counter == 1) {
+          $('.slider').slick('slickNext');
+        }
+          
+      }
+  });
+
+  $('.slider').on('afterChange', function (event, slick, currentSlide) {
+     if ($('.slider-slide-11').hasClass('slick-current')) {
+      $('body').height('auto');
+     }
+  });
 })
