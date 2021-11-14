cea023c64b0427e047a9a30b010aa92ec9ea9be9
moudoder <39887984+moudoder@users.noreply.github.com>
2021-11-14 11:16:00 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   new WOW().init();
   
-  alert('x3')
+  alert('x4')
 
   let slide_2 = 1;
   $(".scrol-to").on("click", function (event) {
@@ -100,7 +100,7 @@ $(document).ready(function () {
   $(window).scroll(function() {
     let scroll = $(window).scrollTop() + $(window).height();
     var offset = $element.offset().top + $element.height();
-    offset = offset - 100;
+    offset = offset - 10;
     if (scroll > offset && counter == 0) {
 
       $('html').addClass('html-over_2');
