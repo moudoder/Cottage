8b853117daee813f38bd62369376d1657b9060dd
moudoder <39887984+moudoder@users.noreply.github.com>
2021-11-14 12:10:00 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   new WOW().init();
   
-  alert('y2')
+  alert('y21')
 
   let slide_2 = 1;
   $(".scrol-to").on("click", function (event) {
@@ -152,7 +152,7 @@ $(document).ready(function () {
         $('body').removeClass('body-over');
         $('html').removeClass('html-over_2');
         $('html').addClass('body-top-2');
-        $('html, body').animate({scrollTop: 2},1);
+        $('html, body').animate({scrollTop: 61},1);
         scrl_tr = 1;
         slide_2 = 1;
        }
